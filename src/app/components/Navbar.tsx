635237019bee893dcc3adec26379607acc8551fa
ilivegod <sekajunior2014@gmail.com>
2025-06-09 13:00:21 +0000

    navbar done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,8 @@ import { useState } from "react";
 const links = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
-  { href: "/faq", label: "FAQ" },
+  { href: "/services", label: "Services" },
+  { href: "/solutions", label: "Solutions" },
   { href: "/contact", label: "Contact" },
 ];
 
@@ -19,18 +20,26 @@ export function Navbar() {
 
   return (
     <div className="relative">
-      <nav className="bg-white lg:rounded-xl lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 mt-6 py-2 px-3 z-50 w-full lg:max-w-[90vh]">
-        <div className="flex items-center justify-between">
+      <nav className="bg-[#121212] lg:flex lg:justify-center py-3 px-3  w-full ">
+        <div className="flex items-center justify-between lg:max-w-5xl w-full ">
           {/* Logo */}
-          <div className="flex items-center gap-2">
+          <div className=" items-center hidden lg:flex gap-2">
             <Image
               className="dark:invert"
               src="/svg/logo.svg"
               alt="ehr logo"
-              width={20}
-              height={20}
+              width={180}
+              height={180}
             />
-            <p className="font-semibold text-lg">EHR SYSTEM</p>
+          </div>
+          <div className="flex items-center lg:hidden gap-2">
+            <Image
+              className="dark:invert"
+              src="/svg/logo.svg"
+              alt="ehr logo"
+              width={130}
+              height={130}
+            />
           </div>
 
           {/* Hamburger (mobile only) */}
@@ -43,26 +52,24 @@ export function Navbar() {
                 <X className="w-6 h-6" />
               ) : (
                 <Image
-                  src="/svg/menu-3-fill.svg"
+                  src="/svg/hamburgerMenu.svg"
                   alt="hamburger"
-                  width={20}
-                  height={20}
+                  width={30}
+                  height={30}
                 />
               )}
             </button>
           </div>
 
           {/* Links (desktop only) */}
-          <ul className="hidden lg:flex gap-5 items-center">
+          <ul className="hidden lg:flex gap-5 items-center ">
             {links.map(({ href, label }) => (
               <li key={href} className="list-none">
                 <Link
                   href={href}
                   className={clsx(
-                    "text-md font-normal",
-                    pathname === href
-                      ? "text-[#0E121B] underline"
-                      : "text-[#0E121B] hover:underline"
+                    "text-sm font-medium text-white",
+                    pathname === href ? " underline" : " hover:underline"
                   )}
                 >
                   {label}
@@ -70,22 +77,6 @@ export function Navbar() {
               </li>
             ))}
           </ul>
-
-          {/* Buttons (desktop only) */}
-          <div className="hidden lg:flex">
-            <Link
-              href="#"
-              className="ml-4 bg-white hover:bg-white hover:text-black hover:underline text-md font-normal text-[#0E121B] px-4 py-2 rounded-lg flex items-center gap-2 transition-colors"
-            >
-              Login
-            </Link>
-            <Link
-              href="#"
-              className="ml-4 bg-[#00A991] hover:bg-[#3da697] text-white px-4 text-sm py-2 rounded-lg flex items-center gap-2 transition-colors"
-            >
-              Get Started <ArrowRight className="w-5 h-5" />
-            </Link>
-          </div>
         </div>
 
         {/* Dropdown absolutely positioned */}
@@ -109,22 +100,6 @@ export function Navbar() {
                 </li>
               ))}
             </ul>
-            <div className="flex flex-col gap-3">
-              <Link
-                href="#"
-                className="bg-white hover:underline text-md font-normal text-[#0E121B] px-4 py-2 rounded-lg transition-colors text-left"
-                onClick={() => setMenuOpen(false)}
-              >
-                Login
-              </Link>
-              <Link
-                href="#"
-                className="bg-[#00A991] hover:bg-[#3da697] text-white px-4 py-2 rounded-lg flex items-center justify-center gap-2 transition-colors"
-                onClick={() => setMenuOpen(false)}
-              >
-                Get Started <ArrowRight className="w-5 h-5" />
-              </Link>
-            </div>
           </div>
         )}
       </nav>
